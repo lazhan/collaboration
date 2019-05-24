88a03328f9e3e482b246964863f501d17c750150
sei <fishclock@naver.com>
2019-05-24 14:12:09 +0900

    ㅈㄹㅈㄹㅈㄹㅈㄹ

--- HelloGit/src/com/kh/git/HelloGit.java
+++ HelloGit/src/com/kh/git/HelloGit.java
@@ -3,7 +3,7 @@ package com.kh.git;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("안녕, Gtt!");
+		System.out.println("작업 fuck you!!");
 		
 		System.out.println("메롱");
 	}
